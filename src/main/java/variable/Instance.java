e2590db7b67ea034a9a9c4d960847237778b9341
lilott8 <lilott8@gmail.com>
2016-09-30 11:04:53 -0700

    variables have been implemented.

--- src/main/java/variable/Instance.java
+++ src/main/java/variable/Instance.java
@@ -1,7 +1,32 @@
 package variable;
 
+import substance.Substance;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jason on 2016/09/29.
  */
 public class Instance implements Variable {
+
+	private String id;
+	private String name;
+	private Map<String, Substance> substances = new HashMap<String, Substance>();
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public Map<String, Substance> getSubstance() {
+		return this.substances;
+	}
+
+	public Variable getReference() {
+		return this;
+	}
 }
--- src/main/java/variable/Reference.java
+++ src/main/java/variable/Reference.java
@@ -1,7 +1,31 @@
 package variable;
 
+import substance.Substance;
+
+import java.util.Map;
+
 /**
  * Created by jason on 2016/09/29.
  */
 public class Reference implements Variable {
+
+	private String id;
+	private String name;
+	private Variable reference;
+
+	public String getName() {
+		return this.name;
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public Map<String, Substance> getSubstance() {
+		return this.reference.getSubstance();
+	}
+
+	public Variable getReference() {
+		return this.reference;
+	}
 }
